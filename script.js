5c620520369aa13f51d0fef95b525a00734cc614
Anshika Bundela <99082647+LAV1E@users.noreply.github.com>
2023-12-15 08:42:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-function fibonacci(num) {
+
 // your code here
+	
+
+function fibonacci(num) {
+    if (num == 1)
+        return 0;
+    if (num == 2)
+        return 1;
+    return fibonacci(num - 1) + fibonacci(num - 2);
 }
+
 
 module.exports = fibonacci;
